3e1dbc25cee76efb88f52ebbe08f72280df6a095
Charan Appala <146429908+cherry6804@users.noreply.github.com>
2024-11-06 20:44:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['src/main.tsx'], // Add this path to external modules
     },
   },
 });
