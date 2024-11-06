f696a15cff2887a4c913dedf5651466aa1aa595e
Charan Appala <146429908+cherry6804@users.noreply.github.com>
2024-11-06 20:46:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
+      // No need to externalize local modules unless absolutely necessary
       external: [],
     },
   },
